411ecc765c5faf553469095f0743316dba578f5f
Billy <billypun8@gmail.com>
2022-06-15 00:19:34 +0800

    init project

---
+++ ping.js
@@ -0,0 +1,24 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+const { token, clientId, serverId } = require('./config.json');
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+];
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(Routes.applicationGuildCommands(clientId, serverId), { body: commands });
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
